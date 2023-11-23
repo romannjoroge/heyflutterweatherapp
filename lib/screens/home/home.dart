fb2d3eac8058939e4ee503e31cd228463f9b6698
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-23 23:03:50 +0300

    Adding image as background of app and placing first text

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:hey_flutter_weather_app/helpers/city_data.dart';
+
+class Home extends StatelessWidget {
+  final City selectedCity;
+
+  const Home({
+    required this.selectedCity,
+    super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // Image of City
+        Image.asset(getImage(City.paris)),
+        // Name of city
+        Positioned(
+          top: 30,
+          left: 30,
+          child: Row(
+            children: [
+              const Icon(Icons.access_alarm, color: Colors.white,),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Text(
+                  getCityName(selectedCity),
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                )
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
