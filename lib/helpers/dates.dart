1fd0cd1d6c7bfa2560cebaf42cf999bb805c14dd
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-24 18:15:52 +0300

    adding functions for getting and setting the latest date

--- lib/helpers/dates.dart
+++ lib/helpers/dates.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 String getMonthDateStringFromDate(DateTime date) {
   var dateFormatter = DateFormat.MMMMd();
@@ -9,3 +10,28 @@ String getMonthYearDateAndTime(DateTime date) {
   var dateFormatter = DateFormat.yMd().add_jm();
   return dateFormatter.format(date);
 }
+
+String getWeekdayAndDay(DateTime date) {
+  var dateFormatter = DateFormat("EEE d");
+  return dateFormatter.format(date);
+}
+
+const lastUpdatedKey = "LAST_UPDATED";
+
+// Sets the provided time as the last updated of the app
+Future<void> setLastUpdated(DateTime lastUpdated) async{
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString(lastUpdatedKey, lastUpdated.toIso8601String());
+}
+
+Future<DateTime> getLastUpdated() async{
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  var lastUpdated = prefs.getString(lastUpdatedKey);
+  if (lastUpdated == null) {
+    await setLastUpdated(DateTime.now());
+    return DateTime.now();
+  } else {
+    return DateTime.parse(lastUpdated);
+  }
+}
+
