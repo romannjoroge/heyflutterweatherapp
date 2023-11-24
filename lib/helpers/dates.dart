03824a1116e0eeb871d98e8bbf37a8dbb912bb86
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-24 20:07:19 +0300

    fix: m was for minutes but I wanted M for months

--- lib/helpers/dates.dart
+++ lib/helpers/dates.dart
@@ -37,7 +37,7 @@ Future<DateTime> getLastUpdated() async{
 
 // The weather API requires dates to be passed as strings with the format yyyy-mm-dd
 String formatDateToWeatherAPIStandard(DateTime date) {
-  var dateFormatter = DateFormat('yyyy-mm-dd');
+  var dateFormatter = DateFormat('yyyy-MM-dd');
   return dateFormatter.format(date);
 }
 
