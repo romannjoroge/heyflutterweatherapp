8565895ef8d89360ed8334e04fb880c44a8e237c
Roman Njoroge <roman.njoroge@njuguna.com>
2023-11-24 16:38:22 +0300

    adding the next days weather to the home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hey_flutter_weather_app/helpers/city_data.dart';
 import 'package:hey_flutter_weather_app/helpers/weather_conditions.dart';
+import 'package:hey_flutter_weather_app/models/weather_conditions.dart';
 import 'package:hey_flutter_weather_app/screens/home/widgets/date_and_last_updated.dart';
 import 'package:hey_flutter_weather_app/screens/home/widgets/location_name_and_dropdown.dart';
+import 'package:hey_flutter_weather_app/screens/home/widgets/next_days_weather.dart';
 import 'package:hey_flutter_weather_app/screens/home/widgets/weather_description.dart';
 import 'package:hey_flutter_weather_app/screens/home/widgets/weather_statistics.dart';
 
@@ -45,10 +47,17 @@ class Home extends StatelessWidget {
                     ),
                     const SizedBox(height: 50),
                     const WeatherConditionWidget(
-                        condition: WeatherCondition.clear, temperature: 24.0),
+                        condition: WeatherCondition.clouds, temperature: 24.0),
                     const SizedBox(height: 50),
                     const WeatherStatisticsWidget(
                         humidity: 56, wind: 4.63, temp: 22),
+                    const SizedBox(height: 20,),
+                    NextDaysWeatherWidget(
+                      day1Weather: NextDaysWeather(date: DateTime.now(), weatherCondition: WeatherCondition.clouds, temperature: 22, bottomWindSpeed: 1, topWindSpeed: 5),
+                      day2Weather: NextDaysWeather(date: DateTime.now(), weatherCondition: WeatherCondition.clear, temperature: 25, bottomWindSpeed: 1, topWindSpeed: 5),
+                      day3Weather: NextDaysWeather(date: DateTime.now(), weatherCondition: WeatherCondition.thunderstorm, temperature: 23, bottomWindSpeed: 5, topWindSpeed: 10),
+                      day4Weather: NextDaysWeather(date: DateTime.now(), weatherCondition: WeatherCondition.mist, temperature: 25, bottomWindSpeed: 1, topWindSpeed: 5),
+                    )
                   ],
                 ),
               )),
